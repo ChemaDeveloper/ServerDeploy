3a62ebe5b0bb33e9f6acf7f7c093dc80288f322b
ChemaDeveloper <jose.rivera.dev@gmail.com>
2018-03-01 16:24:21 +0000

    Version 1.1

--- serverdeploy.sh
+++ serverdeploy.sh
@@ -4,7 +4,7 @@ apt-get -y update
 apt-get -y upgrade
 apt-get -y install php
 apt-get -y install apache2
-apt-get -y install mysql
+apt-get -y install mysql-server
 apt-get -y install git
 apt-get -y install python 
 apt-get clean
